f3fef4605268ba1058257f775de17f2f0995639f
13123913 <perelygin_0505@mail.ru>
2023-04-03 01:35:27 +0700

    recursion

---
+++ laba5_recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace laba5_recursion
+{
+    class Program
+    { 
+            //static double Tangens(double x) => Math.Sin(x) / Math.Cos(x);
+
+            //static void Main(string[] args)
+            //{
+            //   string numbers = Console.ReadLine();
+            //   double number = Double.Parse(numbers);
+            //
+            //   Console.WriteLine("c = " + (number * Tangens(number) + Tangens(number)));
+             
+            /// ///////////////////////////////////////////////////////////////////////////////////////////
+            //public static class Converter
+            //{
+            //    public static double DegreesToRadians(double degrees)
+            //    {
+            //       return degrees * Math.PI / 180.0;
+            //   }
+            //}
+            //   static void Main(string[] args)
+            //  {
+            //      double degrees = 10.0;
+            //      double radians = Converter.DegreesToRadians(degrees);
+             //     Console.WriteLine("{0} degrees is {1} radians.", degrees, radians);
+             // }
+            //////////////////////////////////////////////////////////////////////////////////////////
+            //public static int CountDigits(int x)
+            //{
+            //    int count = 0;
+             //   while (x != 0)
+             //   {
+              //      count++;
+              //      x /= 10;
+              //  }
+              //  return count;
+            //}
+     } 
+ }
+
+
